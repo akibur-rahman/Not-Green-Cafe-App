e525b5473165cd81b1af05ff6eae672890ebdeb8
Akibur Rahman <me.akiburrahman@gmail.com>
2023-12-20 20:58:10 +0600

    Update api.py

--- api/api.py
+++ api/api.py
@@ -44,6 +44,44 @@ def get_menu_items():
         if connection:
             connection.close()
 
+# Route to get all orders using raw SQL query
+
+
+@app.route('/orders', methods=['GET'])
+def get_orders_route():
+    return get_orders()
+
+
+def get_orders():
+    try:
+        connection = mysql.connector.connect(
+            host='localhost', user='root', password='', database='notgreencafe')
+        cursor = connection.cursor(dictionary=True)
+
+        query = "SELECT order_id, customer_id, item_id, quantity, total_price, status FROM orders"
+        cursor.execute(query)
+        orders = cursor.fetchall()
+
+        orders_list = []
+        for order in orders:
+            orders_list.append({
+                'order_id': order['order_id'],
+                'customer_id': order['customer_id'],
+                'item_id': order['item_id'],
+                'quantity': order['quantity'],
+                'total_price': order['total_price'],
+                'status': order['status']
+            })
+
+        return jsonify(orders_list)
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+    finally:
+        if connection:
+            connection.close()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
